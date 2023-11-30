5d4ce1801f2bf6cb1c6468225eacb3dfea6fe54a
victoressien <victoressien1998@gmail.com>
2023-11-30 14:37:23 +0100

    Documentation:Add docstrings to functions

--- main2.py
+++ main2.py
@@ -32,22 +32,60 @@ data = {
 }
 
 
+def create_dir(dir_name: str) -> None:
+    """
+    Create a directory with the specified name.
+
+    Parameters:
+    - dir_name (str): The name of the directory to be created.
+
+    Returns:
+    None
+
+    If the directory already exists, no action is taken.
+    """
+    try:
+        os.mkdir(dir_name)
+    except IOError:
+        pass
+
 def copy_file(fn: str, src: str, dst_dir: str) -> None:
+    """
+    Copies a file from a source location to a destination directory.
+
+    Parameters:
+    - fn (str): The file name (including extension) to be copied.
+    - src (str): The source file path.
+    - dst_dir (str): The destination directory path.
+
+    Returns:
+    None
+
+    If the destination directory does not exist, it will be created before
+    copying the file. The function uses the current working directory (CWD)
+    as the base path for constructing the full destination path.
+    """
     if not os.path.isdir(dst_dir):
         create_dir(dst_dir)
     dst = os.path.join(CWD, dst_dir, fn)
     shutil.copy(src, dst)
 
 
-def create_dir(dir_name: str) -> None:
-    try:
-        os.mkdir(dir_name)
-    except IOError:
-        pass
-
-
 def save_log(file_name: str, content: str) -> None:
+    """
+    Save log content to a file.
 
+    Parameters:
+    - file_name (str): The name of the file to save the log to.
+    - content (str): The log content to be saved.
+
+    Returns:
+    None
+
+    This function attempts to open the specified file in 'append' mode
+    and writes the log content to it. If an IOError or PermissionError
+    occurs, the function continues trying until it is successful.
+    """
     while True:
         try:
             with open(file_name, "a", encoding="utf-8") as output_file:
@@ -58,10 +96,21 @@ def save_log(file_name: str, content: str) -> None:
 
 
 def validate_json(json_data: dict, schema: dict) -> tuple[bool, str]:
+    """
+    Validate JSON data against a JSON schema.
 
-    try:  # try to validate the JSON against schema
-        # print(json_data)
-        # print(schema)
+    Parameters:
+    - json_data (dict): The JSON data to be validated.
+    - schema (dict): The JSON schema used for validation.
+
+    Returns:
+    tuple[bool, str]: A tuple containing a boolean indicating
+    whether the validation succeeded and a string message.
+    If successful, the boolean is True, and the message is 'success'.
+    If validation fails, the boolean is False, and the message
+    contains details about the validation error.
+    """
+    try:  
         validate(instance=json_data, schema=schema)
         return True, 'success'
     except ValidationError as e:
@@ -69,6 +118,16 @@ def validate_json(json_data: dict, schema: dict) -> tuple[bool, str]:
 
 
 def format_time(seconds: float) -> str:
+    """
+    Format time in seconds into a human-readable string.
+
+    Parameters:
+    - seconds (float): The time duration in seconds.
+
+    Returns:
+    str: A formatted string representing the time in days, hours, minutes, and seconds.
+    If the input is None, returns '-'.
+    """
     if seconds is not None:
         seconds = int(seconds)
         d = seconds // (3600 * 24)
@@ -87,6 +146,17 @@ def format_time(seconds: float) -> str:
     
 
 def get_field_names(schema: dict) -> tuple[list, list]:
+    """
+    Retrieve field names for CSV header from a JSON schema.
+
+    Parameters:
+    - schema (dict): The JSON schema.
+
+    Returns:
+    tuple[list, list]: A tuple containing two lists:
+    - The first list represents payload fields for the CSV header.
+    - The second list represents metadata fields for the CSV header.
+    """
     # retrieve fields for CSV header from JSON schema
     schema_copy = schema.copy()  # copy dict to prevent modification of referenced dict argument
     payload_fields = []
@@ -100,8 +170,37 @@ def get_field_names(schema: dict) -> tuple[list, list]:
     return payload_fields, metadata_fields
 
 
+def fix_job_field(job: str) -> str:
+    """
+    Modify a job title for consistent formatting.
+
+    Parameters:
+    - job (str): The original job title.
+
+    Returns:
+    str: The modified job title with consistent formatting.
+    """
+    general_job_title, specialization = job.split(',')
+
+    specialization = specialization.lstrip()  # strip whitespace from specialization
+    new_job_title = f'{specialization} {general_job_title}'.capitalize()  # create new title & capitalize
+
+    return new_job_title
+
+
 def get_row_data(json_data: dict) -> tuple[dict, dict]:
-    # retrieve JSON data for CSV row
+    """
+    Retrieve data for a CSV row from JSON data.
+
+    Parameters:
+    - json_data (dict): The JSON data.
+
+    Returns:
+    tuple[dict, dict]: A tuple containing two dictionaries:
+    - The first dictionary represents payload data for the CSV row.
+    - The second dictionary represents metadata for the CSV row.
+    """
+    
     json_data_copy = json_data.copy()
     payload_dict = dict()
     metadata_dict = dict()
@@ -114,7 +213,7 @@ def get_row_data(json_data: dict) -> tuple[dict, dict]:
 
     # adhoc data fix for "users"
     if 'address' in payload_dict.keys():  # check if is "users" data to prevent unnecessary overhead for "cards" data
-        payload_dict['address'] = payload_dict['address'].replace('\n', ' ')  # strip '\n' (newline character) from address feild
+        payload_dict['address'] = payload_dict['address'].replace('\n', ' ')  # strip '\n' (newline character) from address field
         if ',' in payload_dict['job']:  # check if is affected data
             payload_dict['job'] = fix_job_field(payload_dict['job'])
 
@@ -122,16 +221,17 @@ def get_row_data(json_data: dict) -> tuple[dict, dict]:
     return payload_dict, metadata_dict
 
 
-def fix_job_field(job: str) -> str:
-    general_job_title, specialization = job.split(',')
-
-    specialization = specialization.lstrip()  # strip whitespace from specialization
-    new_job_title = f'{specialization} {general_job_title}'.capitalize()  # create new title & capitalize
-
-    return new_job_title
-
-
 def get_error_log(file_name: str, err_message: str) -> str:
+    """
+    Create an error log entry.
+
+    Parameters:
+    - file_name (str): The name of the file associated with the error.
+    - err_message (str): The error message.
+
+    Returns:
+    str: A formatted error log entry string.
+    """
     time_ = datetime.now().strftime("%d/%m/%Y %I:%M:%S %p")
     error = f'{time_}, ERROR, SCHEMA ERR, {file_name}, {err_message}'
     return error
@@ -149,7 +249,6 @@ def main() -> None:
         data_dir = v['data_dir']
         bad_data_dir = os.path.join(CWD, v['schema_mismatch_dir'])
 
-        # print(schema_file)
 
         file_count = 0  # total JSON data files
         valid_count = 0  # files that match the schema spec
